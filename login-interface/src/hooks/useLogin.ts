2c5175c0135ca0fba90b799bde69c08aaeacf60f
k-lila <krishnalilapalandi@gmail.com>
2026-03-31 18:36:39 -0300

    configuração nginx

--- login-interface/src/hooks/useLogin.ts
+++ login-interface/src/hooks/useLogin.ts
@@ -1,13 +1,15 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useNavigate } from 'react-router'
 import { login } from '../api/authAPI'
 
 export const useLogin = () => {
   const navigate = useNavigate()
+  const queryClient = useQueryClient()
   return useMutation({
     mutationFn: login,
     onSuccess: (data) => {
       localStorage.setItem('token', data.token)
+      queryClient.invalidateQueries({ queryKey: ['currentUser'] })
       navigate('/dashboard')
     },
   })
