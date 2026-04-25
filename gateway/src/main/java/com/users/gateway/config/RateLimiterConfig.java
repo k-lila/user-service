8acfaaf1181c0e0bda71f137d9273408727ec765
k-lila <krishnalilapalandi@gmail.com>
2026-04-25 18:25:39 -0300

    Rate limiter implementado

--- gateway/src/main/java/com/users/gateway/config/RateLimiterConfig.java
+++ gateway/src/main/java/com/users/gateway/config/RateLimiterConfig.java
@@ -1,22 +1,53 @@
 package com.users.gateway.config;
 
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import reactor.core.publisher.Mono;
 
 @Configuration
 public class RateLimiterConfig {
     
+    @Primary
+    @Bean
+    public RedisRateLimiter redisRateLimiterHigh() {
+        return new RedisRateLimiter(10, 20);
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiterMed() {
+        return new RedisRateLimiter(5, 10);
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiterLow() {
+        return new RedisRateLimiter(2, 5);
+    }
+
+    @Primary
     @Bean
     public KeyResolver ipKeyResolver() {
         return exchange -> {
+            var headers = exchange.getRequest().getHeaders();
+            String ip = headers.getFirst("X-Forwarded-For");
+            if (ip != null) {
+                return Mono.just(ip.split(",")[0]);
+            }      
             var address = exchange.getRequest().getRemoteAddress();
-            if (address == null) {
-                return Mono.just("unknown");
-            }
+            if (address == null) return Mono.just("unknown");
             return Mono.just(address.getAddress().getHostAddress());
         };
     }
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange ->
+            exchange.getPrincipal()
+                .map(principal -> principal.getName())
+                .defaultIfEmpty("anonymous");
+    }
+
 }
--- gateway/src/main/java/com/users/gateway/routing/GatewayRouter.java
+++ gateway/src/main/java/com/users/gateway/routing/GatewayRouter.java
@@ -1,5 +1,6 @@
 package com.users.gateway.routing;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -12,36 +13,62 @@ import org.springframework.context.annotation.Configuration;
 public class GatewayRouter {
 
 	@Bean
-	public RouteLocator routes(RouteLocatorBuilder builder, KeyResolver ipKeyResolver) {
+	public RouteLocator routes(RouteLocatorBuilder builder,
+                                @Qualifier("redisRateLimiterLow") RedisRateLimiter redisRateLimiterLow,
+                                @Qualifier("redisRateLimiterMed") RedisRateLimiter redisRateLimiterMed,
+                                @Qualifier("redisRateLimiterHigh") RedisRateLimiter redisRateLimiterHigh,
+                                @Qualifier("ipKeyResolver") KeyResolver ipKeyResolver,
+                                @Qualifier("userKeyResolver") KeyResolver userKeyResolver
+                            ) {
 		return builder.routes()
-		    .route("user-service", route -> route.path("/users/**")
-            .filters(f -> f.requestRateLimiter(c -> {
-                c.setRateLimiter(new RedisRateLimiter(1, 2));
-                c.setKeyResolver(ipKeyResolver);
-            }))
-            .uri("lb://user-service"))
 
+            .route("user-register", route -> route
+                .path("/users/register")
+                .filters(f -> f.requestRateLimiter(c -> {
+                    c.setRateLimiter(redisRateLimiterLow); // mais restritivo
+                    c.setKeyResolver(ipKeyResolver);
+                }))
+                .uri("lb://user-service")
+            )
 
-        // ##### swagger
-        .route("user-service-docs", r -> r
+            .route("oauth", route -> route
+                .path("/oauth2/**")
+                .filters(f -> f.requestRateLimiter(c -> {
+                    c.setRateLimiter(redisRateLimiterMed); // restritivo
+                    c.setKeyResolver(ipKeyResolver);
+                }))
+                .uri("lb://authorization-server")
+            )
+        
+            .route("user-service", route -> route
+                .path("/users/**")
+                .filters(f -> f.requestRateLimiter(c -> {
+                    c.setRateLimiter(redisRateLimiterHigh); //menos restritivo
+                    c.setKeyResolver(userKeyResolver);
+                }))
+                .uri("lb://user-service")
+            )
+
+            // ##### swagger
+            .route("user-service-docs", r -> r
             .path("/v3/api-docs/user/**", "/v3/api-docs/user/")
             .filters(f -> f.rewritePath(
                 "/v3/api-docs/user(?<segment>/?.*)",
                 "/v3/api-docs${segment}"
             ))
             .uri("lb://user-service")
-        )
+            )
 
-        .route("authorization-server-docs", route -> route
+            .route("authorization-server-docs", route -> route
             .path("/v3/api-docs/authorization-server/**", "/v3/api-docs/authorization-server/")
             .filters(f -> f.rewritePath(
                 "/v3/api-docs/authorization-server(?<segment>/?.*)",
                 "/v3/api-docs${segment}"
             ))
             .uri("lb://authorization-server")
-        )
+            )
 
-        .build();
+            .build();
     }
 
 }
